4a1090dcdd328704e271ca207656389597ea0382
Itai Fish <chessandpotatoes@gmail.com>
2023-01-23 16:01:59 -0500

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+	overrides: [
+		{
+			parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+			plugins: ['@typescript-eslint'],
+			extends: [
+				'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+			],
+			rules: {
+				'@typescript-eslint/no-empty-interface': [
+					'off',
+					{
+						allowSingleExtends: true,
+					},
+				],
+				'no-use-before-define': 'off',
+				'@typescript-eslint/no-explicit-any': 'off',
+				'@typescript-eslint/interface-name-prefix': 'off',
+				'@typescript-eslint/no-use-before-define': ['error'],
+				'@typescript-eslint/explicit-module-boundary-types': 'off',
+			},
+			files: ['*.ts', '*.tsx'],
+			parserOptions: {
+				project: 'tsconfig.json',
+			},
+		},
+	],
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'plugin:react/recommended', // React reccoemndations
+		'plugin:@next/next/recommended', // Next
+		'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+		'next/core-web-vitals',
+		'plugin:@typescript-eslint/recommended',
+	],
+	rules: {
+		'@typescript-eslint/consistent-type-imports': 'warn',
+		'prettier/prettier': ['error', { useTabs: true }],
+	},
+};
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,60 @@
+import NextAuth, { type NextAuthOptions } from 'next-auth';
+import DiscordProvider from 'next-auth/providers/discord';
+import GoogleProvider from 'next-auth/providers/google';
+import RedditProvider from 'next-auth/providers/reddit';
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
+import { env } from '../../../env/server.mjs';
+import { prisma } from '../../../server/db';
+
+export const authOptions: NextAuthOptions = {
+	// Include user.id on session
+	callbacks: {
+		session({ session, user }) {
+			if (session.user) {
+				session.user.id = user.id;
+			}
+			return session;
+		},
+	},
+	// Configure one or more authentication providers
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		DiscordProvider({
+			clientId: env.DISCORD_CLIENT_ID,
+			clientSecret: env.DISCORD_CLIENT_SECRET,
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+			authorization: {
+				params: {
+					prompt: 'consent',
+					access_type: 'offline',
+					response_type: 'code',
+				},
+			},
+		}),
+		RedditProvider({
+			clientId: process.env.REDDIT_CLIENT_ID,
+			clientSecret: process.env.REDDIT_CLIENT_SECRET,
+			authorization: {
+				params: {
+					duration: 'permanent',
+				},
+			},
+		}),
+		/**
+		 * ...add more providers here
+		 *
+		 * Most other providers require a bit more work than the Discord provider.
+		 * For example, the GitHub provider requires you to add the
+		 * `refresh_token_expires_in` field to the Account model. Refer to the
+		 * NextAuth.js docs for the provider you want to use. Example:
+		 * @see https://next-auth.js.org/providers/github
+		 */
+	],
+};
+
+export default NextAuth(authOptions);
