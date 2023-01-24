69cbff55838c8b807a8d7de65ddcf35c911e0d19
Itai Fish <chessandpotatoes@gmail.com>
2023-01-24 18:48:36 -0500

    Adding better login flow, going to add game support soon (and then work on cypress e2e)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import styles from './index.module.css';
 import { type NextPage } from 'next';
 import Head from 'next/head';
-import Link from 'next/link';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import background from '../../public/endless-constellation.svg';
 
 import { api } from '../utils/api';
 
-const AuthShowcase: React.FC = () => {
+const LoginButton: React.FC = () => {
 	const { data: sessionData } = useSession();
 
 	const { data: baseData } = api.base.getBaseData.useQuery();
@@ -17,6 +17,11 @@ const AuthShowcase: React.FC = () => {
 				{sessionData && <span>Logged in as {sessionData.user?.name}</span>}
 				{baseData && <> {baseData} </>}
 			</p>
+			{sessionData && (
+				<button className={styles.loginButton} onClick={() => {}}>
+					Enter Game
+				</button>
+			)}
 			<button className={styles.loginButton} onClick={sessionData ? () => void signOut() : () => void signIn()}>
 				{sessionData ? 'Sign out' : 'Sign in'}
 			</button>
@@ -32,13 +37,13 @@ const Home: NextPage = () => {
 				<meta name="description" content="The next best thing" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<main className={styles.main}>
+			<main className={styles.main} style={{ backgroundImage: `url(${background.src})` }}>
 				<div className={styles.container}>
 					<h1 className={styles.title}>
 						Play <span className={styles.pinkSpan}>Node</span>World
 					</h1>
 					<div className={styles.showcaseContainer}>
-						<AuthShowcase />
+						<LoginButton />
 					</div>
 				</div>
 			</main>
